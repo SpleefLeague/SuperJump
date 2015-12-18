321f0dbeba7c468d48ca7e379619cfc080b6c0ff
Jonas Balsfulland <jonas@balsfulland.net>
2015-12-18 19:17:13 +0100

    Exception fix

--- src/main/java/com/spleefleague/superjump/listener/EnvironmentListener.java
+++ src/main/java/com/spleefleague/superjump/listener/EnvironmentListener.java
@@ -43,15 +43,17 @@ public class EnvironmentListener implements Listener {
         SLPlayer slp = SpleefLeague.getInstance().getPlayerManager().get(event.getPlayer());
         if(slp != null && slp.getState() == PlayerState.IDLE) {
             SJPlayer sp = SuperJump.getInstance().getPlayerManager().get(event.getPlayer());
-            for(Arena arena : Arena.getAll()) {
-                if(!sp.getVisitedArenas().contains(arena)) {
-                    if(arena.getArea() != null && arena.getArea().isInArea(event.getTo())) {
-                        sp.getVisitedArenas().add(arena);
-                        String title = ChatColor.GREEN + "You have discovered " + ChatColor.RED + arena.getName() + ChatColor.GREEN + "!";
-                        String subtitle = ChatColor.GRAY + String.valueOf(sp.getVisitedArenas().size()) + "/" + String.valueOf(Arena.getAll().size()) + ChatColor.GOLD + " SJ arenas found!";
-                        PlayerUtil.sendTitle(event.getPlayer(), title, subtitle, 10, 40, 10);
-                        event.getPlayer().playSound(event.getTo(), Sound.FIREWORK_BLAST, 1, 0);
-                        break;
+            if(sp != null) {
+                for(Arena arena : Arena.getAll()) {
+                    if(!sp.getVisitedArenas().contains(arena)) {
+                        if(arena.getArea() != null && arena.getArea().isInArea(event.getTo())) {
+                            sp.getVisitedArenas().add(arena);
+                            String title = ChatColor.GREEN + "You have discovered " + ChatColor.RED + arena.getName() + ChatColor.GREEN + "!";
+                            String subtitle = ChatColor.GRAY + String.valueOf(sp.getVisitedArenas().size()) + "/" + String.valueOf(Arena.getAll().size()) + ChatColor.GOLD + " SJ arenas found!";
+                            PlayerUtil.sendTitle(event.getPlayer(), title, subtitle, 10, 40, 10);
+                            event.getPlayer().playSound(event.getTo(), Sound.FIREWORK_BLAST, 1, 0);
+                            break;
+                        }
                     }
                 }
             }
